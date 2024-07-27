9e936df3e007e3f65f5d4e3456a6f54ccdd04c1d
SanuliGehara <geharasanuli5@gmail.com>
2024-07-27 22:44:30 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+
+dotenv.config();
+
+//connect db
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Mongoose");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
