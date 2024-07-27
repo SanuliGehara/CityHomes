0f740b93accd00bb2568e5798d9f83332678b4fc
SanuliGehara <geharasanuli5@gmail.com>
2024-07-27 21:55:41 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">City</span>
-            <span className="text-slate-700">Home</span>
+            <span className="text-slate-700">Homes</span>
           </h1>
         </Link>
 
