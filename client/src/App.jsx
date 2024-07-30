412b4e20df79e1511456b6dc19260211ccee9298
SanuliGehara <geharasanuli5@gmail.com>
2024-07-30 10:26:43 +0530

    complete sign in page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
+import SignIn from "./pages/Signin";
 
 export default function App() {
   return (
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,76 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Signin() {
-  return <div>SignIn</div>;
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        setError(data.message);
+        setIsLoading(false);
+        return;
+      }
+      setIsLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className="border focus:outline-gray-200 p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border focus:outline-gray-200 p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {isLoading ? "Loading..." : "Sign in"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-700 hover:underline">Sign up</span>
+        </Link>
+      </div>
+
+      {/* Display Error */}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
