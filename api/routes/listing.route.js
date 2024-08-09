a6259f34c28e4058f30c4fc9fab42880aa6590a3
SanuliGehara <geharasanuli5@gmail.com>
2024-08-09 14:38:00 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
   deleteUserListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,5 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteUserListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
