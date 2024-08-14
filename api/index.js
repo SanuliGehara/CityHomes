ed1e75309dbb959424d024d9e19cb5610adb15c7
SanuliGehara <geharasanuli5@gmail.com>
2024-08-14 14:50:55 +0530

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ mongoose
     console.log(error);
   });
 
+const _dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -30,6 +33,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client", "dist", "index.html"));
+});
 
 // Create a middlware to handle errors
 app.use((err, req, res, next) => {
